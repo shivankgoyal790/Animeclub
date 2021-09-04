ce6bf543536b9894c91aaaf7c23675f51d25bd89
shivank goyal <goyal.shivank790@gmail.com>
2021-09-04 13:51:10 +0530

    main files

---
+++ animeclub-frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Mainheader from "./Shared/Mainheader";
+import News from "./Shared/News/News"
+import Footer from "./Shared/Footer"
+function App() {
+  return (
+    <div>
+        <Mainheader />
+        <News/>
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
