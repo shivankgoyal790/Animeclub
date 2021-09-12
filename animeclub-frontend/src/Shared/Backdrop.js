2f004452402af2f0b4cfd6566848e82afd28ad8c
shivank goyal <goyal.shivank790@gmail.com>
2021-09-12 22:03:46 +0530

    backdrop added

---
+++ animeclub-frontend/src/Shared/Backdrop.js
@@ -0,0 +1,11 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import "./Backdrop.css"
+
+const Backdrop = (props) =>{
+    return ReactDOM.createPortal(
+        <div className="backshadow" onClick = {props.onClick}></div>,document.getElementById('backdrop-hook')
+    )
+}
+
+export default Backdrop
