e6dde72c6993ac25d4fb2122e65d84d3563cdc81
shivank goyal <goyal.shivank790@gmail.com>
2021-09-19 23:55:12 +0530

    added loadingspinner

---
+++ animeclub-frontend/src/Shared/Loadingspinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Loadingspinner.css"
+const Lodaingspinner = (props) =>{
+    return(
+        <div className="loading-container">
+                <div className="spinner"></div>
+        </div>
+    )
+}
+
+export default Lodaingspinner
