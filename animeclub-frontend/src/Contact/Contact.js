963d47e7d310b149d899a5f356b9a8af8842a4da
shivank goyal <goyal.shivank790@gmail.com>
2021-09-20 23:11:52 +0530

    chatbot ui created

--- animeclub-frontend/src/Contact/Contact.js
+++ animeclub-frontend/src/Contact/Contact.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import "./Contact.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCommentDots } from "@fortawesome/free-solid-svg-icons";
-import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 
 const Contact = () =>{
@@ -22,15 +23,17 @@ const Contact = () =>{
         <div className="messagebox" style={{display: !showicon ? "block" : "none"}}>
             <h1 className="messagebox-header">
                 <div>Chatbot</div>
-                <div>
-                    <FontAwesomeIcon className="headericons" icon={faWindowClose} />
+                <div onClick={showhandler} style={{cursor:"pointer"}}>
+                    <FontAwesomeIcon className="headericons" icon={faTimes} />
                 </div>
             </h1>
             <div className="chat">
-
+                    <div className="usermessage">Hi There!</div>
+                    <div className="adminmessage">hello</div>
             </div>
             <div className="inputbox">
-                <input type="text" placeholder="enter your message"></input>
+                <input type="text" placeholder="Type your message Here"></input>
+                <div className="sendbtn"><FontAwesomeIcon icon={faPaperPlane} /></div>
             </div>
         </div>
         </>
