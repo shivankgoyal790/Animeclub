b2ecfb8c6bc1f648fcd4fc4db8c3ccc85db0bc7f
shivank goyal <goyal.shivank790@gmail.com>
2021-09-26 23:02:54 +0530

    changed routes

--- animeclub-backend/routes/Item-routes.js
+++ animeclub-backend/routes/Item-routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
  const router = express.Router();
 const Itemcontrollers= require("../Controllers/item-controllers");
+const fileUpload = require("../middlewares/file-upload");
 
-router.post("/createitem" , Itemcontrollers.Createitem);
+router.post("/createitem" ,fileUpload.single('image'), Itemcontrollers.Createitem);
 router.get("/anime",Itemcontrollers.getanime);
 router.get("/movies",Itemcontrollers.getmovies);
 
--- animeclub-backend/routes/News-routes.js
+++ animeclub-backend/routes/News-routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
  const router = express.Router();
- const Newscontrollers = require("../Controllers/News-controllers")
+ const Newscontrollers = require("../Controllers/News-controllers");
+const fileUpload = require("../middlewares/file-upload");
 
  router.get("/" ,Newscontrollers.getnewsbytime);
- router.post("/createnews",Newscontrollers.createnews);
+ router.post("/createnews",fileUpload.single('image'),Newscontrollers.createnews);
  router.patch("/like/:newsid",Newscontrollers.updatecount);
 
 
